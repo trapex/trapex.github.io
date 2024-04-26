f2e4c7b97d6b936cb3074b37ddd891823176f74d
tatianaanosova <anosova@hypeauditor.com>
2024-04-26 17:15:23 +0200

    add mail

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+$recepient = "tanya.pmk@pmk.com"; //Почта на которую будут уходить заявки
+$sitename = "TrendTales"; //Название сайта
+//
+//
+$name = trim($_POST["name"]); //Создаем переменную $name и помещаем в неё значения из поля формы 'name'
+$mail = trim($_POST["email"]); //Создаем переменную $mail и помещаем в неё значения из поля формы 'email'
+$text = trim($_POST["text"]); //Создаем переменную $mail и помещаем в неё значения из поля формы 'email'
+//Если надо добавить ещё поля в форму, нужно добавить переменную по примеру 8-й строки и 
+//её же в выражение на 12-й строке
+//
+$message = "\Name: $name \nEmail: $mail\Message: $text"; //Создаем шаблон письма
+$pagetitle = "Новая заявка с сайта \"$sitename\""; //Задаем тему письма
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+if($message){
+	echo "Thank you! Your submission has been received!"
+    // header("Location: https://www.sitename.com/thank-you");  //В случае успешной отправки перенаправляем пользователя на заранее созданную страницу с благодарностью
+  }else{  //В случае ошибки выводим такое сообщение:
+    echo "Oops! Something went wrong while submitting the form";
+  }
+?>
